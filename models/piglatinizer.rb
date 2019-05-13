d9c5fb4b332609786a67d3066139c9a4bfe19450
Arthur Song <arth3rs0ng@gmail.com>
2019-05-13 23:30:19 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,14 @@
+class PigLatinizer
+  attr_accessor :text, :latinized_text
+  
+  def initialize (text)
+    @text = text
+    @latinized_text = latinize
+  end
+  
+  def latinize 
+    
+  end
+  
+  
+end
