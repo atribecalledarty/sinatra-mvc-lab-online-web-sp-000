bacb47aecf4da0077c915562c9dc4302a57341c7
Arthur Song <arth3rs0ng@gmail.com>
2019-05-14 00:06:21 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -26,6 +26,7 @@ class PigLatinizer
         first_chunk = first_chunk[0..-2]
         word_without_first_chunk = word[index_of_vowel..-1]
         pig_word = word_without_first_chunk + first_chunk + "ay"
+      end
     end
     pig_words.join(" ")
   end
